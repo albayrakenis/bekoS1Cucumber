f0d763486658ec70c03a5379b3fff345e7750ae8
Enis <albayrakenis>
2022-11-30 15:54:35 +0300

    adres ekleme güncel

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -1,35 +1,22 @@
 package step_definitions;
 
 import io.cucumber.java.Scenario;
-import org.junit.After;
-import org.junit.Before;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import pages.BasePage;
-import utility.ConfigurationReader;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import utility.Driver;
 
 import java.util.concurrent.TimeUnit;
 
 public class Hooks {
 
+    @Before
+    public void setUp(){
+       // Driver.get().manage().window().maximize();
+        Driver.get().manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); }
 
-//        @Before
-//        public void setUp(){
-//            Driver.get().get(ConfigurationReader.get("demoblaze.url"));
-//            Driver.get().manage().window().maximize();
-//            Driver.get().manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-//        }
-
-        @After
-        public void tearDown(Scenario scenario){
-            if(scenario.isFailed()){
-                final byte[] screenshot = ((TakesScreenshot)Driver.get()).getScreenshotAs(OutputType.BYTES);
-                scenario.attach(screenshot,"image/png","screenshot");
-            }
-
-            Driver.closeDriver();
-        }
-
-
+    @After
+    public void tearDown()
+    {
+        Driver.closeDriver();
+    }
 }
