38f2774d15e859366a0772c4145b73c7b1ab2f5b
Enis <albayrakenis>
2022-12-01 10:30:03 +0300

    adres silme

---
+++ src/test/java/pages/Hesabim.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Hesabim extends BasePage{
+
+    @FindBy(xpath = "//div[@class='msg msg-warning js-ov-target js-info-popup activated']//button[@class='btn btn-outline-dark js-popup-trigger-func']")
+    public WebElement silmeyiOnayliyormusunuzButonu;
+
+    @FindBy(xpath = "//div[@class='usr-addresses']/div[1]//a[contains(.,'Sil')]")
+    public WebElement ilkAdresSilButonu;
+
+    @FindBy(css = "#modal-add-new-address .icon")
+    public WebElement adresEklemePopupKapatmaButonu;
+
+}
