2e9fbee74eba2fe0fbd2561afda9b90455676baf
Enis <albayrakenis>
2022-12-28 14:42:33 +0300

    smoke test

--- src/test/java/pages/Hesabim.java
+++ src/test/java/pages/Hesabim.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class Hesabim {
 
-    @FindBy(xpath = "//div[@class='msg msg-warning js-ov-target js-info-popup activated']//button[@class='btn btn-outline-dark js-popup-trigger-func']")
+    @FindBy(css = ".js-popup-trigger-func")
     public WebElement silmeyiOnayliyormusunuzButonu;
 
     @FindBy(xpath = "//div[@class='usr-addresses']/div[1]//a[contains(.,'Sil')]")
