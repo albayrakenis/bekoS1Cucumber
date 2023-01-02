61dd5a9fe5935a82aa1fec7fe34db883759f7bdf
Enis <albayrakenis>
2023-01-02 11:43:16 +0300

    cukes runner

---
+++ src/test/java/step_definitions/Test23_TesteHazirlik.java
@@ -0,0 +1,17 @@
+package step_definitions;
+
+import io.cucumber.java.en.When;
+import pages.Anasayfa;
+import utility.BrowserUtils;
+
+public class Test23_TesteHazirlik {
+
+    Anasayfa anasayfa =new Anasayfa();
+    @When("Kullanici sepete gitmeli")
+    public void kullanici_sepete_gitmeli() {
+
+        BrowserUtils.waitFor(2);
+        BrowserUtils.clickWithJS(anasayfa.anasayfaSepetIcon);
+
+    }
+}
---
+++ src/test/resources/features/aatesteHazirlik.feature
@@ -0,0 +1,8 @@
+Feature: teste baslamadan once sepetin bosaltilmasi
+
+  @hazirlik
+  Scenario: Sepette urun olmamali
+    Given Kullanici giris sayfasina gitmeli
+    When Kullanici login olmali
+#    And Kullanici sepete gitmeli
+    And Kullanici sepeti bosalt tusuna basabilmeli
